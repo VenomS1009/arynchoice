54a4d0523bb4245898241e4112bf72825362f6ac
VenomS1009 <eddi.brook@yandex.ru>
2026-02-16 23:02:55 +0300

    Временно вернул SQLite для запуска

--- pro_j/settings.py
+++ pro_j/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'pro_j.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
