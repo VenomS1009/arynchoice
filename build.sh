3146c05b023af7cd41d0ed8577693f9ada5755dc
VenomS1009 <eddi.brook@yandex.ru>
2026-02-17 04:27:45 +0300

    Добавил скрипт сборки

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+if username and password and email:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print('Суперпользователь создан.')
+    else:
+        print('Суперпользователь уже есть.')
+"
